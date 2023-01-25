ac206e9ad304656fcb85914ac564b10e18440c1b
AVETISSIAN Arthur <aavetissian@test.fr>
2023-01-25 17:05:55 +0100

    udpate

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var EmailTitle = document.getElementById("EmailTitle")
 var Email =  document.getElementById("Email")
 
+var NumeroTitle = document.getElementById("NumeroTitle")
+var Numero = document.getElementById("Numero")
 
+var PermisB_title =document.getElementById("Permis B_title")
+var Permis =document.getElementById("Permis B")
 
 
 EmailTitle.onmouseover = function(event){
     Email.style.display = "inline";
 }
 
-EmailTitle.onmouseout = function(event){
-    Email.style.display = "none";
+
+NumeroTitle.onmouseover = function(event){
+    Numero.style.display = "inline";
 }
+
+PermisB_title.onmouseover = function(event){
+    Permis.style.display = "inline";
+}
+
